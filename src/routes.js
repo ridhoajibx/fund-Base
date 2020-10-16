8c167925d8045188d705cbe100f3ef251afcd529
ridhoajibx <ridhoajibx@gmail.com>
2020-10-17 05:49:51 +0700

    [master]--template base master

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import Home from "views/Home.js";
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/Tables.js";
+import UserPage from "views/User.js";
+
+
+var routesMain = [
+    {
+        path: "/home",
+        name: "Home",
+        component: Home,
+        layout: "/main",
+      },
+]
+
+var routesDashboard = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-bank",
+    component: Dashboard,
+    layout: "/account",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "nc-icon nc-diamond",
+    component: Icons,
+    layout: "/account",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "nc-icon nc-bell-55",
+    component: Notifications,
+    layout: "/account",
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/account",
+  },
+  {
+    path: "/tables",
+    name: "Table List",
+    icon: "nc-icon nc-tile-56",
+    component: TableList,
+    layout: "/account",
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: "nc-icon nc-caps-small",
+    component: Typography,
+    layout: "/account",
+  },
+];
+export {routesDashboard, routesMain};
